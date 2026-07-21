19e724266da347d63d59149f671cbc9c1c292cb2
praneethkapilavai <praneethkapilavai@gmail.com>
2026-07-22 00:28:16 +0530

    implemented 2 way communication

--- server.js
+++ server.js
@@ -5,11 +5,22 @@ const {Server} = require('socket.io')
 
 const app = exp()
 const server = http.createServer(app)
+const io = new Server(server)
+
 app.use(exp.static(path.resolve('./public')));
 
 
 server.listen(3000 , ()=>{
     console.log("Server is listening...")
+})
+
+
+io.on('connection' , (socket)=>{
+    console.log('a user connected')
+    socket.on("msgfromF" , (msg)=>{
+        console.log(msg)
+        socket.emit("msgfromB", msg)
+    })
 })
 
 app.get("/" ,(req , res)=>{
